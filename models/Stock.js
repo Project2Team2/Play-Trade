2a2819c379068adb6d33d945478d75355540460c
microvac23 <Ithanmachuca@gmail.com>
2023-07-10 21:49:03 -0500

    Co-authored-by: sangelito <sangelito@users.noreply.github.com> Co-authored-by: Peter Conenna <pconenna@users.noreply.github.com> Co-authored-by: northgoddard <northgoddard@users.noreply.github.com> 1st

---
+++ models/Stock.js
@@ -0,0 +1,51 @@
+// stock model
+// price, amount of shares owned
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Stock extends Model{}
+
+Stock.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+        price: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+            validate:{
+            isDecimal: true
+            }
+        },
+        // shares_owned: {
+        //     type: DataTypes.INTEGER,
+        //     allowNull: false,
+        //     validate:{
+        //         isInt: true
+        //     }
+        // },
+        /* user_id:{
+            type: DataTypes.INTEGER,
+            references:{
+                model: 'user',
+                key: 'id'
+            }
+        } */
+         
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'stock',
+      }
+)
+module.exports = Stock;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,30 @@
 const User = require('./User');
+const Stock = require('./Stock');
+const OwnedStock = require('./OwnedStock');
 
-module.exports = { User };
+// a stock belongs to a user
+// this may need to be stock belongs to many user
+// use the OwnedStock model as a through table
+
+
+// Stock.belongsTo(User,{
+//     foreignKey: 'user_id',
+// })
+
+// this sounds more practical
+Stock.belongsToMany(User,{
+    through: OwnedStock,
+    foreignKey: 'stock_id'
+/*     foreignKey: 'owner_id',
+ */});
+
+// a user can have many stocks
+// this is 100% i checked the sequelize docs
+User.belongsToMany(Stock,{
+    through: OwnedStock,
+    foreignKey: 'owner_id'
+   
+/*     foreignKey: 'user_id',
+ */});
+
+module.exports = { User, Stock, OwnedStock };
