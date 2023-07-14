47488828ac01bbf516338985fa73798a99ad2be3
sangelito <129316135+sangelito@users.noreply.github.com>
2023-07-14 16:01:23 -0500

    Login page and main view

--- public/js/script.js
+++ public/js/script.js
@@ -8,3 +8,30 @@ searchBtn.addEventListener("click", ()=>{
     const searchvalue = document.querySelector('#search-input').value
     
 });
+
+//toggle button function NOT WORKING
+document.addEventListener('DOMContentLoaded', function () {
+    var sidebarToggle = document.getElementById('sidebarToggle');
+    var layoutSidenav = document.getElementById('layoutSidenav');
+    
+    sidebarToggle.addEventListener('click', function (event) {
+        event.preventDefault();
+        
+        layoutSidenav.classList.toggle('sb-sidenav-toggled');
+    });
+});
+
+//pres log in button get routed to main.handlebars NOT WORKING
+document.addEventListener('DOMContentLoaded', function () {
+    var loginForm = document.getElementById('loginForm');
+    
+    loginForm.addEventListener('submit', function (event) {
+        event.preventDefault(); // Prevent form submission
+        
+        // Get the form action URL
+        var action = loginForm.getAttribute('action');
+        
+        // Redirect to the specified URL
+        window.location.href = action;
+    });
+});
